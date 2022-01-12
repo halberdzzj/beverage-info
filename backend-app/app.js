cc861608f294cfee86edaca93c6220e718adbb03
Zhiji Zhu <zhiji.zhu@gmail.com>
2022-01-13 00:55:42 +0800

    to start task 2

--- backend-app/app.js
+++ backend-app/app.js
@@ -9,22 +9,34 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//     return res.json({ result: 'working !', data: new Date().toISOString() });
-// });
-
 app.get('/drinks', async (req, res) => {
 
     try {
         const type = req.query.type;
         // console.log('app', type)
         const data = await fetchDrinks(type);
-        return res.send({drinks: data});
+        return res.send({ drinks: data });
 
     } catch (err) {
         return res.send({ Message: err.Message });
     }
-})
+});
+
+app.get('/cafes', (req, res) => {
+
+});
+
+app.get('/cafes/employees', (req, res) => {
+
+});
+
+app.post('/cafe', (req, res) => {
+
+});
+
+app.post('/cafe/employee', (req, res) => {
+
+});
 
 app.listen(PORT, () => {
     console.log('app is running!');
